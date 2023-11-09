e7a9116bcd436cf0b34a6c080da95d91ef968c58
Matheus Ferreira <matheusscfr@gmail.com>
2023-11-09 00:49:39 -0300

    Pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import {Routes ,Route} from 'react-router-dom'
+import Home from './core/pages/Home'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/states' element={<h1>States</h1>}/>
+      <Route path='/country/:isoCode' element={<h1>Country</h1>}/>
+      <Route path='*' element={<h1>Not found</h1>}/>
+    </Routes>
   );
 }
 
